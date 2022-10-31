75f92784321de406194aeb7377c0bcedf1742ee7
ekg1229 <ekg1229@naver.com>
2022-10-31 16:19:21 +0900

    practice2

---
+++ homework/work10/practice2/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ContextHook from "./chapter_14/ContextHook"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <ContextHook/>
+  </div>
+);
